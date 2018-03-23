98f7178f8e30f3d175ba79254f5176b9aa0d7e9d
Ty Carter <ty.carter5028@gmail.com>
2018-03-23 17:59:56 -0400

    fixes error, formatting

--- lambda_function.py
+++ lambda_function.py
@@ -5,51 +5,55 @@ POC lambda interface for Alexa Skill
 
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
-	return {
-		'outputSpeech': {
-			'type': 'PlainText',
-			'text': output
-		},
-		'card': {
-			'type': 'Simple',
-			'title': 'SessionSpeechlet - ' + title,
-			'content': 'SessionSpeechlet - ' + output
-		},
-		'reprompt': {
-			'outputSpeech': {
-				'type': 'PlainText',
-				'text': reprompt_text
-			}
-		},
-		'shouldEndSession': should_end_session
-	}
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': 'SessionSpeechlet - ' + title,
+            'content': 'SessionSpeechlet - ' + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
 
 def build_response(session_attributes, speechlet_response):
-	return {
-		'version': '0.1',
-		'sessionAttributes': session_attributes,
-		'response', speechlet_response
-	}
+    return {
+        'version': '0.1',
+        'sessionAttributes': session_attributes,
+        'response', speechlet_response
+    }
+
 
 def run_game():
-	session_attributes = {}
-	card_title = 'Welcome'
-	speech_output = 'Under Construction'
-	reprompt_text = None
-	should_end_session = True
-	return build_response(
-		session_attributes,
-		build_speechlet_response(
-			card_title,
-			speech_output,
-			reprompt_text,
-			should_end_session
-		)
-	)
+    session_attributes = {}
+    card_title = 'Welcome'
+    speech_output = 'Under Construction'
+    reprompt_text = None
+    should_end_session = True
+    return build_response(
+        session_attributes,
+        build_speechlet_response(
+            card_title,
+            speech_output,
+            reprompt_text,
+            should_end_session
+        )
+    )
+
 
 def on_launch(launch_request, session):
-	return get_welcome_response()
+    return get_welcome_response()
+
 
 def lambda_handler(event, context):
-	if event['request']['type'] == 'LaunchRequest':
-		return on_launch(event['request'], event['session'])
+    if event['request']['type'] == 'LaunchRequest':
+        return on_launch(event['request'], event['session'])
